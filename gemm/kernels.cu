6c711373fa1f9047434f1e167677905af661756c
Pranav Kumar Kota <ppranavkumarkota@gmail.com>
2025-01-06 14:31:11 +0000

    (add gemm kernels + test)

---
+++ gemm/kernels.cu
@@ -0,0 +1,65 @@
+
+__global__ void naive_kernel(int M, int N, int K, float *A, float *B, float *C) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row < M && col < N) {
+        float sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += A[row * K + i] * B[i * N + col];
+        }
+        C[row * N + col] = sum;
+    }
+}
+
+
+__global__ void shared_kernel(int M, int N, int K, float *A, float *B, float *C){
+    /*
+    It is assumde that the block is square. To generalize validity, add additional checks (TBD)
+    */
+
+    int globalRow = blockIdx.y*blockDim.y + threadIdx.y;
+    int globalCol = blockIdx.x*blockDim.x + threadIdx.x;
+
+    int localRow = threadIdx.y;
+    int localCol = threadIdx.x;
+
+    // define shared memory to load tiles of input matrices A & B
+    extern __shared__ float sm[];
+    float *ATile = sm;
+    float *BTile = sm + blockDim.x*blockDim.y;
+
+    // thread sum for output index
+    float sum = 0.0f;
+    int phases = (K + blockDim.x - 1)/blockDim.x;
+
+    if(globalRow==0 && globalCol==0){
+        printf("Phases: %d\n", phases);
+    }
+    for(int p=0; p<phases; p++){
+        __syncthreads();
+        // load A into shared memory
+        if(globalRow<M && p*blockDim.x + localCol < K){
+            ATile[localRow*blockDim.x + localCol] = A[globalRow*K + p*blockDim.x + localCol];
+        }
+        else{
+            ATile[localRow*blockDim.x + localCol] = 0.0f;
+        }
+        // load B into shared memory
+        if(p*blockDim.y + localRow < K && globalCol < N){
+            BTile[localRow*blockDim.x + localCol] = B[(p*blockDim.y + localRow)*N + globalCol];
+        }
+        else{
+            BTile[localRow*blockDim.x + localCol] = 0.0f;
+        }
+        // sync to wait for all data to be loaded
+        __syncthreads();
+        // perform matrix multiplication between loaded tiles and update sum
+        for(int j=0; j<blockDim.x; j++){
+            sum += ATile[localRow*blockDim.x + j]*BTile[j*blockDim.x + localCol];
+        }
+    }
+
+    if(globalRow<M && globalCol<N){
+        C[globalRow*N + globalCol] = sum;
+    }
+}
---
+++ gemm/test.py
@@ -0,0 +1,63 @@
+import pycuda.autoinit
+import pycuda.driver as cuda
+import pycuda.gpuarray as gpuarray
+import numpy as np
+from pycuda.compiler import SourceModule
+
+BLOCK = (32, 32, 1)
+GRID = lambda M, N, BLOCK=BLOCK, : (int(np.ceil(M/BLOCK[0])), int(np.ceil(N/BLOCK[1])), 1)
+
+def gen_matrices(M, K, N):
+    A = np.random.randn(M, K).astype(np.float32)
+    B = np.random.randn(K, N).astype(np.float32)
+    return A, B
+
+def cpu_mm(A, B):
+    C = np.dot(A, B)
+    return C
+
+def load_module(file):
+    kernel = r'{}'.format(open(file).read())
+    return SourceModule(kernel)
+
+def naive_mm(A, B, M, K, N, func):
+    M = np.int32(M)
+    N = np.int32(N)
+    K = np.int32(K)
+    C = np.zeros((M, N)).astype(np.float32)
+    func(M, N, K, cuda.In(A), cuda.In(B), cuda.Out(C), block=BLOCK, grid=GRID(M, N))
+    cuda.Context.synchronize()
+    return C
+
+def shared_mm(A, B, M, K, N, func):
+    M = np.int32(M)
+    N = np.int32(N)
+    K = np.int32(K)
+    C = np.zeros((M, N)).astype(np.float32)
+    shared_size = 2 * BLOCK[0] * BLOCK[1] * np.dtype(np.float32).itemsize
+    func(M, N, K, cuda.In(A), cuda.In(B), cuda.Out(C), block=BLOCK, grid=GRID(M, N), shared=shared_size)
+    return C
+
+
+def test(BLOCK=BLOCK, dims=[32, 64, 128, 256, 512, 1024]):
+    mod = load_module('kernels.cu')
+    naive_func = mod.get_function('naive_kernel')
+    shared_func = mod.get_function('shared_kernel')
+    print("*"*20)
+    print(f"BLOCK = {BLOCK}")
+    print("*"*20)
+    for d in dims:
+        M, K, N = d, d, d
+        A, B = gen_matrices(M, K, N)
+        C_naive = naive_mm(A, B, M, K, N, naive_func)
+        C_shared = shared_mm(A, B, M, K, N, shared_func)
+        diff = np.abs(C_naive - C_shared)
+        print(f"dim = {d} : grid = {GRID(M, N)}: avg diff = {np.mean(diff):.3f}")
+        try:
+            assert np.allclose(C_naive, C_shared)
+            print(f"\033[32mPassed!\033[0m")
+        except:
+            print(f"\033[31mFailed!\033[0m")
+
+if __name__ == '__main__':
+    test()
